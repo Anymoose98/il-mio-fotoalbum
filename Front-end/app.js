619d41d137ddee794b8cf9544d5affe1d856cd46
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-06-26 09:46:00 +0200

    first commit

---
+++ Front-end/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port= process.env.host || 3000;
+
+app.get('/', (req, res) => {
+    res.send("<h1>Prova<h1>");
+  });
+
+app.listen(port, () => {
+    console.log(`Server http://localhost:${port}`);
+  });
