4502c5c46bdd8778818d1bdab1122778c1ee24ff
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-06-27 14:40:58 +0200

    Rotta privata funzionante

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -5,7 +5,9 @@ import { AuthProvider } from './assets/components/contexts/AuthContext';
 import Header from './assets/components/header/Header';
 import Footer from './assets/components/footer/Footer';
 import HomePage from './assets/components/pages/homepage/HomePage';
-import Login from './assets/components/pages/login/Login'
+import Login from './assets/components/pages/login/Login';
+import PrivatePage from './assets/components/middlewares/PrivatePage';
+import Create from './assets/components/pages/Create/Create';
 // import Show from './assets/components/pages/show/Show'
 
 
@@ -20,7 +22,11 @@ function App() {
             <Route path="/" element={<HomePage />} />
             {/* <Route path="/:id" element={<Show />} /> */}
             <Route path="/login" element={<Login />} />
-            
+
+            <Route path="/create" element={
+              <PrivatePage><Create /></PrivatePage>
+            } />
+
           </Routes>
         </AuthProvider>
         <Footer />
---
+++ Front-end/src/assets/components/pages/Create/Create.jsx
@@ -0,0 +1,19 @@
+import createStyles from "./Create.module.scss"
+
+const Create = () => {
+    return (
+        <main>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <h1>Create</h1>
+                    </div>
+                    
+                </div>
+            </div>
+
+        </main>
+    )
+}
+
+export default Create;
